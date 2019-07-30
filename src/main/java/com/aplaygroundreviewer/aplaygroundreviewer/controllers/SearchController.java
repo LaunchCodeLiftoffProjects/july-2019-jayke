918502fb45396e86c28755e7e10b08b52b119afe
Archetypal <archetypal.0@gmail.com>
2019-07-30 01:45:56 -0500

    Form that return text typed in onto next page. Query parameter displays entered text.

--- src/main/java/com/aplaygroundreviewer/aplaygroundreviewer/controllers/SearchController.java
+++ src/main/java/com/aplaygroundreviewer/aplaygroundreviewer/controllers/SearchController.java
@@ -1,15 +1,36 @@
 package com.aplaygroundreviewer.aplaygroundreviewer.controllers;
 
+import com.aplaygroundreviewer.aplaygroundreviewer.models.forms.SearchForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class SearchController {
 
-    @RequestMapping(value = "")
+    @RequestMapping(value = "", method = RequestMethod.GET)
     public String index(Model model) {
         model.addAttribute("title", "Welcome to Playground Reviewer");
+        model.addAttribute(new SearchForm());
         return "index";
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public String index(Model model, SearchForm searchForm) {
+        String name = searchForm.getName();
+
+        return "redirect:/search?name=" + name;
+    }
+
+    @RequestMapping(value = "search", method = RequestMethod.GET)
+    //public String displaySearch(Model model, String name)
+    public String displaySearch(Model model, HttpServletRequest request) {
+        //model.addAttribute("aname", name);
+        String name = request.getParameter("name");
+        model.addAttribute("aname", name);
+        return "search-results"; //SUCCESSS!!!!!!
     }
 }
---
+++ src/main/java/com/aplaygroundreviewer/aplaygroundreviewer/models/forms/SearchForm.java
@@ -0,0 +1,17 @@
+package com.aplaygroundreviewer.aplaygroundreviewer.models.forms;
+
+import javax.validation.constraints.NotNull;
+
+public class SearchForm {
+
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
