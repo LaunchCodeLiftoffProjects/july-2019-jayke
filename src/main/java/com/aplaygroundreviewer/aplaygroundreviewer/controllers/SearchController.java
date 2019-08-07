607a64d832c6c296484d89f49877e94d07b994eb
Archetypal <archetypal.0@gmail.com>
2019-08-06 20:00:15 -0500

    Updated Title Page to "STL Playgrounds Finder"

--- src/main/java/com/aplaygroundreviewer/aplaygroundreviewer/controllers/SearchController.java
+++ src/main/java/com/aplaygroundreviewer/aplaygroundreviewer/controllers/SearchController.java
@@ -13,7 +13,7 @@ public class SearchController {
 
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String index(Model model) {
-        model.addAttribute("title", "Welcome to STL Playgrounds Finder");
+        model.addAttribute("title", "Welcome to STL Playgrounds Finder!");
         model.addAttribute(new SearchForm());
         return "index";
     }
