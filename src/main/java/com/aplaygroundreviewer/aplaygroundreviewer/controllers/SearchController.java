71ca1dfa9fe94ceaaff11548fb7e656271a24ca1
Archetypal <archetypal.0@gmail.com>
2019-08-07 19:49:23 -0500

    Working on arraylist for Playground object. New Playgrounds will not display on index page.

--- src/main/java/com/aplaygroundreviewer/aplaygroundreviewer/controllers/SearchController.java
+++ src/main/java/com/aplaygroundreviewer/aplaygroundreviewer/controllers/SearchController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
@@ -37,4 +38,18 @@ public class SearchController {
         model.addAttribute("aname", name);
         return "search-results"; //SUCCESSS!!!!!!
     }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddPlaygroundForm(Model model) {
+        model.addAttribute("title", "Add a Playground Object");
+        return "add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddPlaygroundForm(@RequestParam String playgroundName, @RequestParam String playgroundDescription) {
+        Playground newPlayground = new Playground(playgroundName, playgroundDescription);
+        playgrounds.add(newPlayground);
+        return "redirect: ";
+    }
+
 }
