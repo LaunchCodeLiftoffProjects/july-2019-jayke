e95f44ceef0d422932f9cc4d138a7077140ea573
Archetypal <archetypal.0@gmail.com>
2019-08-07 23:28:07 -0500

    ArrayList code successfully refactored to code that is able to interact with a MAMP database via CrudRepository functionality. Currently have "add" handler with error messages to display appropriately. Will continue to work on "remove" handler.

--- src/main/java/com/aplaygroundreviewer/aplaygroundreviewer/controllers/SearchController.java
+++ src/main/java/com/aplaygroundreviewer/aplaygroundreviewer/controllers/SearchController.java
@@ -1,18 +1,26 @@
 package com.aplaygroundreviewer.aplaygroundreviewer.controllers;
 
 import com.aplaygroundreviewer.aplaygroundreviewer.models.Playground;
+import com.aplaygroundreviewer.aplaygroundreviewer.models.data.PlaygroundDao;
 import com.aplaygroundreviewer.aplaygroundreviewer.models.forms.SearchForm;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 import java.util.ArrayList;
 
 @Controller
 public class SearchController {
+
+    @Autowired
+    private PlaygroundDao playgroundDao;
 
     static ArrayList<Playground> playgrounds = new ArrayList<>();
 
@@ -20,7 +28,8 @@ public class SearchController {
     public String index(Model model) {
         model.addAttribute("title", "Welcome to STL Playgrounds Finder");
         model.addAttribute(new SearchForm());
-        model.addAttribute("playgrounds", playgrounds);
+        //model.addAttribute("playgrounds", playgrounds);
+        model.addAttribute("playgrounds", playgroundDao.findAll());
         return "index";
     }
 
@@ -43,13 +52,21 @@ public class SearchController {
     @RequestMapping(value = "add", method = RequestMethod.GET)
     public String displayAddPlaygroundForm(Model model) {
         model.addAttribute("title", "Add a Playground Object");
+        model.addAttribute(new Playground());
         return "add";
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddPlaygroundForm(@RequestParam String playgroundName, @RequestParam String playgroundDescription) {
-        Playground newPlayground = new Playground(playgroundName, playgroundDescription);
-        playgrounds.add(newPlayground);
+    public String processAddPlaygroundForm(@ModelAttribute @Valid Playground newPlayground, Errors errors, Model model) {
+        //Playground newPlayground = new Playground(playgroundName, playgroundDescription);
+        //playgrounds.add(newPlayground);
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Playground");
+            return "add";
+        }
+
+        playgroundDao.save(newPlayground);
         return "redirect:";
     }
 
--- src/main/java/com/aplaygroundreviewer/aplaygroundreviewer/models/Playground.java
+++ src/main/java/com/aplaygroundreviewer/aplaygroundreviewer/models/Playground.java
@@ -1,13 +1,35 @@
 package com.aplaygroundreviewer.aplaygroundreviewer.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Playground {
 
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    @Size(min=3, max=400)
     private String name;
+
+    @NotNull
+    @Size(min=3, max=400)
     private String description;
 
     public Playground(String name, String description) {
         this.name = name;
         this.description = description;
+    }
+
+    public Playground () { }
+
+    public int getId() {
+        return id;
     }
 
     public String getName() {
