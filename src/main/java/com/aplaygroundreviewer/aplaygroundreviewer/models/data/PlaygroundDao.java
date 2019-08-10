5ea3c6107c1e710c252e7625668aba1a191d78f3
Archetypal <archetypal.0@gmail.com>
2019-08-10 07:53:31 -0500

    Successfully able to convert to a JpaRepository. Optimized imports.

--- src/main/java/com/aplaygroundreviewer/aplaygroundreviewer/models/data/PlaygroundDao.java
+++ src/main/java/com/aplaygroundreviewer/aplaygroundreviewer/models/data/PlaygroundDao.java
@@ -2,7 +2,6 @@ package com.aplaygroundreviewer.aplaygroundreviewer.models.data;
 
 import com.aplaygroundreviewer.aplaygroundreviewer.models.Playground;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 public interface PlaygroundDao extends JpaRepository<Playground, Integer> {
 }
