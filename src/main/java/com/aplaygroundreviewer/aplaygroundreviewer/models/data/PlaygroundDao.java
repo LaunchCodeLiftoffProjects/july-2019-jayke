baa15f9bd8d711f29b07293bd3b2f62a735b8d4c
Archetypal <archetypal.0@gmail.com>
2019-08-12 23:12:50 -0500

    issues

--- src/main/java/com/aplaygroundreviewer/aplaygroundreviewer/models/data/PlaygroundDao.java
+++ src/main/java/com/aplaygroundreviewer/aplaygroundreviewer/models/data/PlaygroundDao.java
@@ -10,4 +10,5 @@ public interface PlaygroundDao extends JpaRepository<Playground, Integer> {
 
     List<Playground> findByName(String name);
     List<Playground> findByNameContaining(String name);
+    List<Playground> findByNameOrDescription(String name);
 }
