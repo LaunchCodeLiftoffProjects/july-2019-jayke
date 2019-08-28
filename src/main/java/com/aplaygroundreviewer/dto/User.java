44c5875fcae6b739542a262f5cf6bec415df5e11
Tesia Le <tesiale28@gmail.com>
2019-08-27 20:31:54 -0500

    added getters and setters, fixed db name

--- src/main/java/com/aplaygroundreviewer/dto/User.java
+++ src/main/java/com/aplaygroundreviewer/dto/User.java
@@ -47,4 +47,32 @@ public class User
     {
         this.roles = roles;
     }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
