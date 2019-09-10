1801778b586a9445e0b807a8af0c674aec1a3c54
kamakshiSK <kamakshi.raibagkar@gmail.com>
2019-09-10 00:27:00 -0500

    Added javascript code to put playgrounds markers on Search result page

--- src/main/resources/static/js/renderMap.js
+++ src/main/resources/static/js/renderMap.js
@@ -1,17 +1,22 @@
-function loadMap(location){
+function loadMap(){
   <!--set Map Options -->
       var options = {
-        center: {lat: 38.6270, lng: -90.1994},
         zoom: 9
       };
 
   <!--New map-->
-      map = new google.maps.Map(document.getElementById('map'), options );
+      var map = new google.maps.Map(document.getElementById('map'), options );
 
-  <!-- Get Latitude and Longitude -->
+  <!--Get location to put marker-->
+      var locations = document.querySelectorAll("[id='playgroundName']");
+
+  var bounds= new google.maps.LatLngBounds();
+
+  <!-- Calculate Latitude and Longitude for location -->
+  for (i=0; i!=locations.length; i++){
       axios.get('https://maps.googleapis.com/maps/api/geocode/json',{
                     params:{
-                            address:location,
+                            address:locations[i].value,
                             key:'AIzaSyC7hSUws5hbDKi3JScfBgPEKeGo4EbJb_s'
                             }
                     })
@@ -21,15 +26,28 @@ function loadMap(location){
                     var lng = response.data.results[0].geometry.location.lng;
 
                     <!-- Add marker -->
-                     var marker = new google.maps.Marker({
+                     marker = new google.maps.Marker({
                                 position : {lat: lat, lng: lng},
                                 map : map,
+                                animation: google.maps.Animation.DROP,
                      });
 
                       <!-- Info window for marker -->
-                      var infoWindow = new google.maps.InfoWindow( {content:'<h4> ' + location +' </h4>'} );
+                      var infoWindow = new google.maps.InfoWindow( {content:'<h4> ' + locations +' </h4>'} );
                       marker.addListener('click', function(){
                                 infoWindow.open(map,marker);
                       });
+
+                      <!-- Form a boundary for map -->
+                      bounds = new google.maps.LatLngBounds();
+                      bounds.extend(marker.getPosition());
       });
+
+      }
+                      <!-- Fit all markers on a map -->
+                      map.setCenter(bounds.getCenter());
+                      map.fitBounds(bounds);
+                      map.setZoom(9);
 }
+
+
