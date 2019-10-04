f539630225721d5fa34883565b335521718915e7
Archetypal <archetypal.0@gmail.com>
2019-10-04 17:06:26 -0500

    Successfully able to search by checkedboxed features. Cleaned up code.

--- src/main/java/com/aplaygroundreviewer/controllers/SearchCheckboxController.java
+++ src/main/java/com/aplaygroundreviewer/controllers/SearchCheckboxController.java
@@ -1,16 +1,12 @@
 package com.aplaygroundreviewer.controllers;
-
 import com.aplaygroundreviewer.dto.*;
 import com.aplaygroundreviewer.repository.PlaygroundDao;
-import org.hibernate.mapping.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.persistence.Access;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -20,7 +16,6 @@ public class SearchCheckboxController {
 
     @Autowired
     private PlaygroundDao playgroundDao;
-    //private static HashMap<String, String> playgroundFeatures = new HashMap<>();
 
 
     @RequestMapping(value = "searchbyfeatures", method = RequestMethod.POST)
@@ -39,20 +34,6 @@ public class SearchCheckboxController {
             listsize = ++listsize;
         }
         int listSizeMinusOne = listsize - 1;
-
-
-        //if (listsize == 1) {
-
-            //parking lot
-            //if (playgroundFeaturesArrayList.contains("ParkingLot")) {
-
-            //See if boolean getters as strings successfully convert to useful in if statements.
-            HashMap<Integer, String> testingBooleanGettersAsStrings = new HashMap<>();
-
-
-            //HashMap with filtered checkbox Booleans from pFAL
-            HashMap<Integer, String> filteredBooleanValuesAsStrings = new HashMap<>();
-            Integer anInteger = 0;
 
             for (Playground playground : allPlaygrounds) {
                 Accessibility accessibility = playground.getAccessibility();
@@ -93,111 +74,16 @@ public class SearchCheckboxController {
 
                 //Counter for every .isaB that is "True"
                 for (CheckboxItem checkbox : checkboxItemArrayList) {
-                    //if(checkboxItemArrayList.size() > listSizeMinusOne)
                     if (checkbox.isaBoolean()) {
-                        //continue; //placeholder
-                        // (int variable) = ++ (int variable)
                         counterForCheckboxItemArrayList = ++ counterForCheckboxItemArrayList;
-
                     }
                 }
 
+                //If all CheckboxItem objects in checkboxItemArrayList are "true" (check via counterForCheckboxItemArrayList), add playground object to targeted playgrounds.
                 if (checkboxItemArrayList.size() > listSizeMinusOne && counterForCheckboxItemArrayList == listsize) {
                     targetedPlaygrounds.add(playground);
                 }
-
-                /*if (listsize == 1
-                        && checkboxItemArrayList.size() > 0
-                        && checkboxItemArrayList.get(0).isaBoolean()) {
-                    targetedPlaygrounds.add(playground);
-                } else if (listsize == 2
-                        && checkboxItemArrayList.size() > 1
-                        && checkboxItemArrayList.get(0).isaBoolean()
-                        && checkboxItemArrayList.get(1).isaBoolean()) {
-                    targetedPlaygrounds.add(playground);
-                } else if (listsize == 3
-                        && checkboxItemArrayList.size() > 2
-                        && checkboxItemArrayList.get(0).isaBoolean()
-                        && checkboxItemArrayList.get(1).isaBoolean()
-                        && checkboxItemArrayList.get(2).isaBoolean())  {
-                    targetedPlaygrounds.add(playground);
-                } else if (listsize == 4
-                        && checkboxItemArrayList.size() > 3
-                        && checkboxItemArrayList.get(0).isaBoolean()
-                        && checkboxItemArrayList.get(1).isaBoolean()
-                        && checkboxItemArrayList.get(2).isaBoolean()
-                        && checkboxItemArrayList.get(3).isaBoolean())  {
-                    targetedPlaygrounds.add(playground);
-                } else if (listsize == 5
-                        && checkboxItemArrayList.size() > 4
-                        && checkboxItemArrayList.get(0).isaBoolean()
-                        && checkboxItemArrayList.get(1).isaBoolean()
-                        && checkboxItemArrayList.get(2).isaBoolean()
-                        && checkboxItemArrayList.get(3).isaBoolean()
-                        && checkboxItemArrayList.get(4).isaBoolean())  {
-                    targetedPlaygrounds.add(playground);
-                } else if (listsize == 6
-                        && checkboxItemArrayList.size() > 5
-                        && checkboxItemArrayList.get(0).isaBoolean()
-                        && checkboxItemArrayList.get(1).isaBoolean()
-                        && checkboxItemArrayList.get(2).isaBoolean()
-                        && checkboxItemArrayList.get(3).isaBoolean()
-                        && checkboxItemArrayList.get(4).isaBoolean()
-                        && checkboxItemArrayList.get(5).isaBoolean())  {
-                    targetedPlaygrounds.add(playground);
-                } else if (listsize == 7
-                        && checkboxItemArrayList.size() > 6
-                        && checkboxItemArrayList.get(0).isaBoolean()
-                        && checkboxItemArrayList.get(1).isaBoolean()
-                        && checkboxItemArrayList.get(2).isaBoolean()
-                        && checkboxItemArrayList.get(3).isaBoolean()
-                        && checkboxItemArrayList.get(4).isaBoolean()
-                        && checkboxItemArrayList.get(5).isaBoolean()
-                        && checkboxItemArrayList.get(6).isaBoolean())  {
-                    targetedPlaygrounds.add(playground);
-                }*/
             }
-
-        //}
-        /*else if (listsize == 2) {
-            for (Playground playground : allPlaygrounds) {
-                Accessibility accessibility = playground.getAccessibility();
-                Amenities amenities = playground.getAmenities();
-                Equipment equipment = playground.getEquipment();
-                Shelters shelters = playground.getShelters();
-                SpecialNeeds specialNeeds = playground.getSpecialNeeds();
-                //Water Feature
-
-                //CheckboxItem List
-                ArrayList<CheckboxItem> checkboxItemArrayList = new ArrayList<CheckboxItem>();
-
-                HashMap<String, Boolean> allF = new HashMap<>();
-                allF.put("ParkingLot", accessibility.isParkingLot());
-                allF.put("Slides", equipment.isSlides());
-
-                for (String key : allF.keySet()) {
-                    if (playgroundFeaturesArrayList.contains(key) && allF.get(key)) {
-                        CheckboxItem checkboxItem = new CheckboxItem(0, key, allF.get(key));
-                        checkboxItemArrayList.add(checkboxItem);
-                    }
-                }
-
-                if (checkboxItemArrayList.size() > 1 && checkboxItemArrayList.get(0).isaBoolean() && checkboxItemArrayList.get(1).isaBoolean()) {
-
-                    //String checkboxName = checkboxItemArrayList.get(0).getaString();
-                    //System.out.println("The name is " + checkboxName);
-                    //CheckboxItem checkboxItemZero = checkboxItemArrayList.get(0);
-                    targetedPlaygrounds.add(playground);
-                }
-            }
-
-        } else {
-            System.out.println("Oh.");
-        }*/
-
-        //search for playgrounds that contain slides AND laders
-        //return "redirect:playground/remove";
-
         model.addAttribute(new SearchForm());
         model.addAttribute("playgrounds",targetedPlaygrounds);
         return "searchbylists/listbyfields";
