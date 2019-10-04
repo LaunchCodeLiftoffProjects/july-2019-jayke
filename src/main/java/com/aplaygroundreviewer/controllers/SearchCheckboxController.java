19d5da76b350ef4a40717328c33a81713a8f62e0
Archetypal <archetypal.0@gmail.com>
2019-10-03 19:52:53 -0500

    Am unsure how to refactor code to where code that read "accessibility.isParkingLot" will be saved in a list with these literal characters and then converted to a Boolean or boolean type for if statements. WIP

--- src/main/java/com/aplaygroundreviewer/controllers/SearchCheckboxController.java
+++ src/main/java/com/aplaygroundreviewer/controllers/SearchCheckboxController.java
@@ -40,346 +40,83 @@ public class SearchCheckboxController {
         }
 
 
-
-        //////LENGTH = 2
-        /*if (listsize == 2) {
-
-            //////PARKING LOT
-            //parking lot and trail
-            if (playgroundFeaturesArrayList.contains("parkingLot") && playgroundFeaturesArrayList.contains("trail")) {
-
-                for (Playground playground : allPlaygrounds) {
-                    Accessibility accessibility = playground.getAccessibility();
-                    if (accessibility.isParkingLot() && accessibility.isTrail()) {
-                        targetedPlaygrounds.add(playground);
-                    } else {
-                        continue;
-                    }
-                }
-            }
-
-
-            //parking lot and restroom changing station
-
-            if (playgroundFeaturesArrayList.contains("restroomChangingStation") && playgroundFeaturesArrayList.contains("parkingLot")) {
-
-                for (Playground playground : allPlaygrounds) {
-                    Accessibility accessibility = playground.getAccessibility();
-                    Amenities amenities = playground.getAmenities();
-
-                    if (accessibility.isParkingLot() && amenities.isRestroomChangingStation()) {
-                        targetedPlaygrounds.add(playground);
-                    } else {
-                        continue;
-                    }
-
-                }
-            }
-
-
-            //parking lot and water fountain
-
-            if (playgroundFeaturesArrayList.contains("waterFountain") && playgroundFeaturesArrayList.contains("parkingLot")) {
-
-                for (Playground playground : allPlaygrounds) {
-                    Accessibility accessibility = playground.getAccessibility();
-                    Amenities amenities = playground.getAmenities();
-
-                    if (accessibility.isParkingLot() && amenities.isWaterFountain()) {
-                        targetedPlaygrounds.add(playground);
-                    } else {
-                        continue;
-                    }
-
-                }
-            }
-
-
-            //parking lot and climbing walls
-
-            if (playgroundFeaturesArrayList.contains("parkingLot") && playgroundFeaturesArrayList.contains("climbingWalls")) {
-
-                for (Playground playground : allPlaygrounds) {
-                    Accessibility accessibility = playground.getAccessibility();
-                    Equipment equipment = playground.getEquipment();
-
-                    if (accessibility.isParkingLot() && equipment.isClimbingWalls()) {
-                        targetedPlaygrounds.add(playground);
-                    } else {
-                        continue;
-                    }
-
-                }
-            }
-
-            //parking lot and ladders
-
-            if (playgroundFeaturesArrayList.contains("parkingLot") && playgroundFeaturesArrayList.contains("ladders")) {
-
-                for (Playground playground : allPlaygrounds) {
-                    Accessibility accessibility = playground.getAccessibility();
-                    Equipment equipment = playground.getEquipment();
-
-                    if (accessibility.isParkingLot() && equipment.isLadders()) {
-                        targetedPlaygrounds.add(playground);
-                    } else {
-                        continue;
-                    }
-
-                }
-            }
-
-
-            //parking lot and seesaw
-
-            if (playgroundFeaturesArrayList.contains("parkingLot") && playgroundFeaturesArrayList.contains("seesaw")) {
-
-                for (Playground playground : allPlaygrounds) {
-                    Accessibility accessibility = playground.getAccessibility();
-                    Equipment equipment = playground.getEquipment();
-
-                    if (accessibility.isParkingLot() && equipment.isSeesaw()) {
-                        targetedPlaygrounds.add(playground);
-                    } else {
-                        continue;
-                    }
-
-                }
-            }
-
-
-            //parking lot and slides
-
-            if (playgroundFeaturesArrayList.contains("parkingLot") && playgroundFeaturesArrayList.contains("slides")) {
-
-                for (Playground playground : allPlaygrounds) {
-                    Accessibility accessibility = playground.getAccessibility();
-                    Equipment equipment = playground.getEquipment();
-
-                    if (accessibility.isParkingLot() && equipment.isSlides()) {
-                        targetedPlaygrounds.add(playground);
-                    } else {
-                        continue;
-                    }
-
-                }
-            }
-
-
-            //parking lot and structures
-
-            if (playgroundFeaturesArrayList.contains("parkingLot") && playgroundFeaturesArrayList.contains("structures")) {
-
-                for (Playground playground : allPlaygrounds) {
-                    Accessibility accessibility = playground.getAccessibility();
-                    Equipment equipment = playground.getEquipment();
-
-                    if (accessibility.isParkingLot() && equipment.isStructures()) {
-                        targetedPlaygrounds.add(playground);
-                    } else {
-                        continue;
-                    }
-
-                }
-            }
-
-
-            //parking lot and swings
-
-            if (playgroundFeaturesArrayList.contains("parkingLot") && playgroundFeaturesArrayList.contains("swings")) {
-
-                for (Playground playground : allPlaygrounds) {
-                    Accessibility accessibility = playground.getAccessibility();
-                    Equipment equipment = playground.getEquipment();
-
-                    if (accessibility.isParkingLot() && equipment.isSwings()) {
-                        targetedPlaygrounds.add(playground);
-                    } else {
-                        continue;
-                    }
-
-                }
-            }
-
-
-            //parking lot and benches
-
-            if (playgroundFeaturesArrayList.contains("parkingLot") && playgroundFeaturesArrayList.contains("benches")) {
-
-                for (Playground playground : allPlaygrounds) {
-                    Accessibility accessibility = playground.getAccessibility();
-                    Shelters shelters = playground.getShelters();
-
-                    if (accessibility.isParkingLot() && shelters.isBenches()) {
-                        targetedPlaygrounds.add(playground);
-                    } else {
-                        continue;
-                    }
-
-                }
-            }
-
-
-            //parking lot and pavillion
-
-            if (playgroundFeaturesArrayList.contains("parkingLot") && playgroundFeaturesArrayList.contains("pavillion")) {
-
-                for (Playground playground : allPlaygrounds) {
-                    Accessibility accessibility = playground.getAccessibility();
-                    Shelters shelters = playground.getShelters();
-
-                    if (accessibility.isParkingLot() && shelters.isPavillion()) {
-                        targetedPlaygrounds.add(playground);
-                    } else {
-                        continue;
-                    }
-
-                }
-            }
-
-
-            //parking lot and sensory
-
-            if (playgroundFeaturesArrayList.contains("parkingLot") && playgroundFeaturesArrayList.contains("sensory")) {
-
-                for (Playground playground : allPlaygrounds) {
-                    Accessibility accessibility = playground.getAccessibility();
-                    SpecialNeeds specialNeeds = playground.getSpecialNeeds();
-
-                    if (accessibility.isParkingLot() && specialNeeds.isSensory()) {
-                        targetedPlaygrounds.add(playground);
-                    } else {
-                        continue;
-                    }
-
-                }
-            }
-
-
-            //parking lot and wheelchairAccessible
-
-            if (playgroundFeaturesArrayList.contains("parkingLot") && playgroundFeaturesArrayList.contains("wheelchairAccessible")) {
-
-                for (Playground playground : allPlaygrounds) {
-                    Accessibility accessibility = playground.getAccessibility();
-                    SpecialNeeds specialNeeds = playground.getSpecialNeeds();
-
-                    if (accessibility.isParkingLot() && specialNeeds.isWheelchairAccessible()) {
-                        targetedPlaygrounds.add(playground);
-                    } else {
-                        continue;
-                    }
-
-                }
-            }
-
-
-            //parking lot and water feature
-
-            if (playgroundFeaturesArrayList.contains("parkingLot") && playgroundFeaturesArrayList.contains("waterFeature")) {
-
-                for (Playground playground : allPlaygrounds) {
-                    Accessibility accessibility = playground.getAccessibility();
-
-                    if (accessibility.isParkingLot() && playground.isWaterFeature()) {
-                        targetedPlaygrounds.add(playground);
-                    } else {
-                        continue;
-                    }
-
-                }
-            }
-
-
-            // slides and ladders
-            if (playgroundFeaturesArrayList.contains("slides") && playgroundFeaturesArrayList.contains("ladders")) {
-                //System.out.println("Yes, slides AND ladders");
-                //Create a list of all playgrounds
-
-                //List<Playground> allPlaygrounds = playgroundDao.findAll();
-                //List<Playground> targetedPlaygrounds = new ArrayList<>();
-
-                //For each playground in allPlaygroundsList
-                for (Playground playground : allPlaygrounds) {
-                    //Create Equipment object
-                    Equipment equipment = playground.getEquipment();
-                    //If equipment.isLadders AND equipment.isSlides
-                    if (equipment.isLadders() && equipment.isSlides()) {
-                        //add playground to targetedPlaygrounds list
-                        targetedPlaygrounds.add(playground); //SUCCESS!!!!!!!!!!!!!!!!
-                    //else continue
-                    } else {
-                        System.out.println("Continue");
-                        continue;
-                    }
-                }
-
-            }
-
-
-            //Testing
-            if (playgroundFeaturesArrayList.contains("slides") && playgroundFeaturesArrayList.contains("food")) {
-                System.out.println("Yes, slides AND food");
-            } else {
-                System.out.println("Yeah, no food.");
-            }
-
-
-
-        } else*/
         if (listsize == 1) {
 
-
-            /*for (Playground playground : allPlaygrounds) {
-                for (String item : playgroundFeaturesArrayList) {
-                    if ()
-                }
-
-            }*/
-
             //parking lot
-            if (playgroundFeaturesArrayList.contains("ParkingLot")) {
+            //if (playgroundFeaturesArrayList.contains("ParkingLot")) {
 
+                //See if boolean getters as strings successfully convert to useful in if statements.
+                HashMap<Integer, String> testingBooleanGettersAsStrings = new HashMap<>();
+
+
+                //HashMap with filtered checkbox Booleans from pFAL
+                HashMap<Integer, String> filteredBooleanValuesAsStrings = new HashMap<>();
+                Integer anInteger = 0;
 
                 for (Playground playground : allPlaygrounds) {
                     Accessibility accessibility = playground.getAccessibility();
+                    Amenities amenities = playground.getAmenities();
+                    Equipment equipment = playground.getEquipment();
+                    Shelters shelters = playground.getShelters();
+                    SpecialNeeds specialNeeds = playground.getSpecialNeeds();
+                    //Water Feature
 
-                    //Having working HashMap with Boolean values and strings
-                    HashMap<String, Boolean> booleanValues = new HashMap<>();
-                    booleanValues.put("ParkingLot", accessibility.isParkingLot());
-                    String isParking = "accessibility.isParkingLot()";
-                    boolean varBoolean = Boolean.parseBoolean(isParking); //Return false do to string not being "true"
+                    testingBooleanGettersAsStrings.put(0,"accessibility.isParkingLot()");
 
-                    if (booleanValues.get("ParkingLot")) {
+                    //MAY USE
+                    /*for (String checkbox : playgroundFeaturesArrayList) {
+                        if (checkbox.contains("ParkingLot")) {
+                            filteredBooleanValuesAsStrings.put(anInteger,"accessibility.isParkingLot()");
+                        } else {
+                            continue;
+                        }
+
+                        anInteger = ++anInteger;
+                    }*/
+
+                    /*for (String checkbox : playgroundFeaturesArrayList) {
+                        //if statements (put current anInteger value, and set boolean values)
+                        if (checkbox.contains("ParkingLot")) {
+                            filteredBooleanValues.put(anInteger,accessibility.isParkingLot());
+                        } else {
+                            continue;
+                        }
+
+                        //integer counter increase by 1
+                    }*/
+
+                    //NOT NEEDED, TESTING: add all checkbox Boolean values, later compare this HashMap's key with strings in pFAL
+                    //booleanValues.put("ParkingLot", accessibility.isParkingLot());
+
+                    //Can probably comment out these permanently, still present for a reference
+                    //String isParking = "accessibility.isParkingLot()";
+                    //boolean varBoolean = Boolean.parseBoolean(isParking); //Return false do to string not being "true"
+
+                    //*********Do something similar to this, except...
+                    /*if (booleanValues.get("ParkingLot")) {
                         targetedPlaygrounds.add(playground);
                     } else{
                         continue;
-                    }
+                    }*/
 
+
+                    //...For each Str in pFAL
+                    /*for (String astring : playgroundFeaturesArrayList) {
+                        //For each Key in booleanValues
+                        for (String key : booleanValues.keySet()) {
+                            //if Str in pFAL == Key
+                            if (key.contains(astring)) {
+                                //put Key:Value set in filteredBooleanValues
+                                filteredBooleanValues.put(key, booleanValues.get(key));
+                            }
+
+                        }
+                    }*/
                 }
-            }
-
-
-            //trail
-            if (playgroundFeaturesArrayList.contains("trail")) {
-
-                for (Playground playground : allPlaygrounds) {
-                    Accessibility accessibility = playground.getAccessibility();
-
-                    if (accessibility.isTrail()) {
-                        targetedPlaygrounds.add(playground);
-                    } else {
-                        continue;
-                    }
-
-                }
-            }
-
-
-
+            //}
 
         } else {
-            System.out.println("No, not two");
+            System.out.println("No, listsize not one.");
         }
 
         //search for playgrounds that contain slides AND laders
